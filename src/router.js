09fe20934e457fa5a6fcb5474cf587454ed1abfc
Eric Canales <ecanal16@stu.southtexascollege.edu>
2022-03-09 12:51:13 -0600

    Custom Changes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,6 @@
 import {createRouter, createWebHistory} from "vue-router";
 
 import Index from "./pages/index.vue";
-import Second from "./pages/second.vue";
-import Third from "./pages/third.vue";
-import Fourth from "./pages/fourth.vue";
-import Fifth from "./pages/fifth.vue";
 import NotFound from "./pages/404.vue";
 
 const routes = [
@@ -12,26 +8,6 @@ const routes = [
         path: "/",
         name: "Index",
         component: Index,
-    },
-    {
-        path: "/second",
-        name: "Second",
-        component: Second,
-    },
-    {
-        path: "/third",
-        name: "Third",
-        component: Third,
-    },
-    {
-        path: "/fourth",
-        name: "Fourth",
-        component: Fourth,
-    },
-    {
-        path: "/fifth",
-        name: "Fifth",
-        component: Fifth,
     },
     {
         path: "/:pathMatch(.*)*",
